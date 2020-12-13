0e59e5bcd6f61e4037fc836dc67ac4f3fd0f94f1
Kyle <lookerkj@protonmail.com>
2020-12-12 16:16:43 -0800

    youtube api hooked up and formatted for the video project, working great.'

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,7 +1,19 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = (props) => {
-  return <div>{props.videos.length}</div>;
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onClick={() => console.log("in list")}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
+  });
+
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
